6820cbfd700154bc1c581136d23e6efd6b84da25
Boris Egorov <dolphin278@gmail.com>
2013-09-29 17:00:08 +0400

    Remove some data from test

--- test/integration/test.js
+++ test/integration/test.js
@@ -2,7 +2,7 @@ var Loader = require('../../lib/Loader');
 var Processor = require('../../lib/Processor');
 var Reporter = require('../../lib/Reporter');
 var fs = require('fs');
-var logDir = './test/logs';
+var logDir = './test/logs2';
 var es = require('event-stream');
 
 
